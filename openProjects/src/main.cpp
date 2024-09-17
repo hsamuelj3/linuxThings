7ac55f720344f61547ecc42203d365cd3e895e9a
hsamuelj3 <swagcaptainjojo@gmail.com>
2024-09-17 19:15:22 +0200

    More file editing

---
+++ openProjects/src/main.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+    int decisionVariable;
+    bool decision = true;
+    while (decision == true)
+    {
+        cout << "enter a number 0-10" << endl;
+        cin >> decisionVariable;
+        switch (decisionVariable)
+        {
+            case 0: 
+                break;
+            case 1: 
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
